71cb83bf32252611a6834ae026aedebbfa2d3424
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-04-14 17:47:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import random 
-import colorama
-from colorama import Fore
-from colorama import Style
-from os import system
+import random
+import os
 import sys
 import time
 import random
 import string
 import pathlib
-import requests, os, threading, sys, time, random, ctypes, webbrowser,re, hashlib, datetime, os.path
-import colorama
-from colorama import Fore, init, Back, Style
+import requests, os, time, random, ctypes, webbrowser,re, hashlib, datetime, os.path
+import init, Back, Style
 from datetime import date
 import asyncio
 import codecs
@@ -20,20 +16,36 @@ import random
 import threading
 import requests
 import os
-import colorama
 
  
 import pyfiglet
 from pyfiglet import Figlet
  
-from colorama import Fore, init
+import init
 from datetime import datetime
 from itertools import cycle
  
+#colors--
+black="\033[0;30m"
+red="\033[0;31m"
+bred="\033[1;31m"
+green="\033[0;32m"
+bgreen="\033[1;32m"
+yellow="\033[0;33m"
+byellow="\033[1;33m"
+blue="\033[0;34m"
+bblue="\033[1;34m"
+purple="\033[0;35m"
+bpurple="\033[1;35m"
+cyan="\033[0;36m"
+bcyan="\033[1;36m"
+white="\033[0;37m"
+nc="\033[00m"
+#colors--
 
 while True:
  system("clear")
- print(Fore.CYAN + """
+ print("""{cyan}
  __        __                        
  \ \      / /__ _ __ _ __   ___ _ __ 
   \ \ /\ / / _ \ '__| '_ \ / _ \ '__|
@@ -43,19 +55,19 @@ while True:
 """)                                                    
                                    
  print(Fore.RESET + """Discord Tag: 𝐏𝐡𝐚𝐧𝐭𝟎𝐦 𝐓𝐡𝐞 𝐆𝐫𝐞𝐚𝐭#1150	 
-Replit Profile: https://replit.com/@phant0m007      
+Replit Profile: https://replit.com/@phant0m007{white}      
 """)
  token = input("[>>>] Victim's token: ")
 
  head = {'Authorization': str(token)}
  src = requests.get('https://discordapp.com/api/v6/users/@me', headers=head)
  if src.status_code != 200:
-   print(Fore.RED + '[Invalid Token]')
-   print(Fore.RESET + "Do you want to try again? if yes, start over")
+   print('[Invalid Token]')
+   print("Do you want to try again? if yes, start over")
    exit(0)
  
  system('clear')
- print(Fore.CYAN + """
+ print("""{cyan}
  __        __                        
  \ \      / /__ _ __ _ __   ___ _ __ 
   \ \ /\ / / _ \ '__| '_ \ / _ \ '__|
@@ -64,7 +76,7 @@ Replit Profile: https://replit.com/@phant0m007
                                      
 """)                                                    
                                    
- print(Fore.RESET + """Discord Tag: 𝐏𝐡𝐚𝐧𝐭𝟎𝐦 𝐓𝐡𝐞 𝐆𝐫𝐞𝐚𝐭#1150	 
+ print(f"""{white}Discord Tag: 𝐏𝐡𝐚𝐧𝐭𝟎𝐦 𝐓𝐡𝐞 𝐆𝐫𝐞𝐚𝐭#1150	 
 Replit Profile: https://replit.com/@phant0m007      
 """)
               
@@ -79,12 +91,12 @@ Replit Profile: https://replit.com/@phant0m007
          email = r.json()['email']
          mfa = r.json()['mfa_enabled']
          print(f'''
-[{Fore.CYAN}User ID{Fore.RESET}]         {userID}
-[{Fore.CYAN}User Name{Fore.RESET}]     {userName}
-[{Fore.CYAN}2 Factor{Fore.RESET}]        {mfa}
-[{Fore.CYAN}Email{Fore.RESET}]           {email}
-[{Fore.CYAN}Phone number{Fore.RESET}]    {phone if phone else ""}
-[{Fore.CYAN}Token{Fore.RESET}]           {token}
+[{cyan}User ID{Fore.white}]         {userID}
+[{cyan}User Name{white}]     {userName}
+[{cyan}2 Factor{white}]        {mfa}
+[{cyan}Email{white}]           {email}
+[{cyan}Phone number{white}]    {phone if phone else ""}
+[{cyan}Token{white}]           {token}
 
 ----------------------------------------------
             ''')
